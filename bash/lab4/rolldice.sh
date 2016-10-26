29b2065d59ead2da12cb55e70fe30a79c965d44a
krunal patel <krunalpatel1311@gmail.com>
2016-10-26 19:35:41 +0000

    new rolldice

--- bash/lab4/rolldice.sh
+++ bash/lab4/rolldice.sh
@@ -1,75 +1,68 @@
-#/bin/bash
-#lab 4 script 1 
-#this script prompt the user for count of dice and than ralls them
-#done by krunal patel-200304119
+#!/bin/bash
+# this script gets 2 numbers from the user
+#   a count of dice and a count of sides per die
+#   it prints out the results of rolling those dice
 
 
-###variables
+########### created by krunal patel 200304119
 
+# the number of dice store in $count, defaulting to 2
+# the number of sides store in $sides, defaulting to 6
+# start the total rolled at zero
 declare -i count
 count=0 #number of dies
 sides=0 #number of sides from 4 to 20 only 
 declare -i sides
 
+#function
 
-###fuction 
-
-function error-message {
-    echo "$@" >&2 
-    
+function error-msg()
+{
+    echo "$@" >&2
 }
 
+# Main
 
-##command line proccing 
+# Use command line for checking count and sides
 while [ $# -gt 0 ]; do
     case "$1" in
-    
-    -h )
-        
-        echo "Usage: $0 [-h] [-c numberofdice] [-s numberofsidesperdie] [-s 4-20]"
-        exit 0
-        ;;    
-        
     -c )
-        if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then    
+        if [[ "$2" =~ ^[1-5][0-5]*$ ]]; then
             count=$2
             shift
         else
-            error-message "you gave me '$2' as the number of dice to roll, bad plan muchcho" 
+            error-meg "bad value for '$2'"
             exit 1
         fi
-        
         ;;
-        
-     -s )
-        if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then    
-            if [ $2 -lt 4 -o $2 -gt 20 ]; then
-            echo " number of sides must be from 4 to 20 " > $2
-            
-            sides=$2
-            shift
-            
+    -s )
+        if [[ "$2" =~ ^[1-9][0-9]*$ ]]; then
+            if [ "$2" -lt 4 -o "$2" -gt 20 ]; then
+            echo " slides should be 4 to 20: " > $2
+                sides=$2
+                shift
+            else
+               error-msg "bad value for '$2'"
+                exit 1
             fi
-        else
-            error-message "you gave me '$2' as the number of sides per die, no way that happning" 
-            exit 1
         fi
-        
-        ;;
-    * )
+       ;;
+       
+       -h )
+       echo "Usage: $0 [-h] [-c dices] [-s sides] [-s 4-20]"
+        exit 0
+       exit 2
+       ;;
+       
+        * )
     
-            error-message "i din't understand '$1' " 
-            echo "Usage: $0 [-h] [-c numberofdice] [-s numberofsidesperdie] [-s 4-20]"
+            error-msg "wronge input for '$1' " 
+            echo "Usage: $0 [-h] [-c dices] [-s sides] [-s 4-20]"
             exit 1
         ;;    
-    
     esac
     shift
 done
-
-###user input
-
-#get a valid roll count from user 
 
 until [ $count -gt 0 ]; do
   read -p "How many dice shall I roll[1-5]? " count
@@ -85,8 +78,20 @@ until [ $sides -gt 3 -a $sides -lt 21 ]; do
 done
 ##main
 # do the dice roll as many times as the user asked for
-for (( rolls=0 ; rolls < count ; rolls++ )); do
-# roll the dice
-  die1=$(($RANDOM % $sides +1))
-  echo "Rolled $die1"
+while [ $count -gt 0 ]; do
+
+    # the roll range is based on the number of sides
+    roll=$(( $RANDOM % $sides +1 ))
+    
+    # add the current roll total to the running total
+    sum=$(( $sum + $roll ))
+    
+    # give the user feedback about the current roll
+    echo "Rolled $roll"
+    
+    # the loop will end when the count of dice to roll reaches zero
+    ((count--))
 done
+
+# done rolling, display the sum of the rolls
+echo "Total sum of rolles dice is: $sum"
